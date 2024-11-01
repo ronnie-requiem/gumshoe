1edf443ea1c94c2b9365a19f3f388b9038bb5b3a
ronnie-requiem <raidpliv@gmail.com>
2024-11-01 07:40:00 +0300

    Update apikeys.py

--- apikeys.py
+++ apikeys.py
@@ -1,1 +1,3 @@
-github_api_key=""   #make gihub, get you api key access
+github_api_key=""  #make gihub, get you api key access
+git clone https://github.com/asharbinkhalil/gumshoe.git
+pip install -r requirements.txt
